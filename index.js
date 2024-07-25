81fd91fc4caa5e0294f199ad552ddbab5d3e110e
HongHa123456 <149506091+HongHa123456@users.noreply.github.com>
2024-07-25 19:45:07 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.set('port', process.env.port || 5000);
+
+app.listen(app.get('port'),function(){
+    console.log(`Server listening on port ${app.get('port')}`);
+})
+
